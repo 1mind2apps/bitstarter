98e71f7aee42da173b1995eb26a74c23d39902b6
minghan <1mind2apps@gmail.com>
2013-07-08 12:57:37 +0000

    quotes again

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var content = fs.readFileSync(index.html);  
+	var content = fs.readFileSync('index.html');  
 
 response.send(content);
 });
